31fd4fda27fef25859b9353b49694740ad98fad1
noncasus <noncasus@gmail.com>
2015-02-20 15:20:41 -0600

    fix menu

--- themes/primobiz/functions.php
+++ themes/primobiz/functions.php
@@ -72,7 +72,7 @@ function html5blank_nav()
 		'container'       => '',
 		'container_class' => '',
 		'container_id'    => '',
-		'menu_class'      => 'nav navbar-nav navbar-right',
+		'menu_class'      => 'nav navbar-nav',
 		'menu_id'         => '',
 		'echo'            => true,
 		'fallback_cb'     => 'wp_page_menu',
