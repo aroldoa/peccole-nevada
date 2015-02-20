ef93faa1af24d5b96f02847190190ba4bebfdf25
aroldoa <aroldo@primomotif.com>
2015-02-20 13:27:54 -0600

    Update functions.php

--- themes/peccole-nevada/functions.php
+++ themes/peccole-nevada/functions.php
@@ -1,10 +1,3 @@
 <?php
-/*--------------------------------*\
-	Show WP Admin Bar
-\*--------------------------------*/
 
-add_action('init', 'rem_admin_bar');
-function rem_admin_bar() {
-	if (!current_user_can('administrator') && !is_admin()) show_admin_bar(false);
-}
 ?>
