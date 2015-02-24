afcbb30bf3ba1919e937cef4e48be23b3b44f682
noncasus <noncasus@gmail.com>
2015-02-23 21:33:31 -0600

    maplace fix

--- themes/peccole-nevada/inc/peccole-maplace.js
+++ themes/peccole-nevada/inc/peccole-maplace.js
@@ -1,38 +1,39 @@
-var peccole = new Maplace({
-    locations: [{
-        lat: 29.611417, 
-        lon: -98.607020,
-        icon: 'http://maps.google.com/mapfiles/markerA.png',
-        type:'marker'
-    }],
-    map_options:{
-        set_center: [29.611417, -98.607020],
-        zoom: 15
-    },
-    styles:{
-        'Peccole':[
-        {
-            stylers:[
-                {hue: "#ff1a00"},
-                {invert_lightness: 'true'},
-                {saturation: -100},
-                {lightness: 33},
-                {gamma: 0.5}
-            ]
-        }]
-    }
-    });
 
-    peccole.o.afterCreateMarker = function (index, location, marker) {
-    google.maps.event.addListener(marker, 'click', function() {
-        window.open('http://goo.gl/TRUaSL');
-    });
+    var peccole = new Maplace({
+        locations: [{
+            lat: 29.611417, 
+            lon: -98.607020,
+            icon: 'http://maps.google.com/mapfiles/markerA.png',
+            type:'marker'
+        }],
+        map_options:{
+            set_center: [29.611417, -98.607020],
+            zoom: 15
+        },
+        styles:{
+            'Peccole':[
+            {
+                stylers:[
+                    {hue: "#ff1a00"},
+                    {invert_lightness: 'true'},
+                    {saturation: -100},
+                    {lightness: 33},
+                    {gamma: 0.5}
+                ]
+            }]
+        }
+        });
 
-    google.maps.event.addDomListener(window, 'load', primo.Load());
-    google.maps.event.addDomListener(window, "resize", function() {
-        google.maps.event.trigger(peccole, "resize");
-        peccole.oMap.setCenter(peccole.oMap.getCenter());
-        peccole.Load();
-    });
+        peccole.o.afterCreateMarker = function (index, location, marker) {
+        google.maps.event.addListener(marker, 'click', function() {
+            window.open('http://goo.gl/TRUaSL');
+        });
 
-};
+        google.maps.event.addDomListener(window, 'load', primo.Load());
+        google.maps.event.addDomListener(window, "resize", function() {
+            google.maps.event.trigger(peccole, "resize");
+            peccole.oMap.setCenter(peccole.oMap.getCenter());
+            peccole.Load();
+        });
+
+    });
