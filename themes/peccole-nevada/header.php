4c342842c9aee26587541449e118863f3fbcb905
noncasus <noncasus@gmail.com>
2015-02-24 21:53:26 -0600

    testing error fix

--- themes/peccole-nevada/header.php
+++ themes/peccole-nevada/header.php
@@ -56,7 +56,7 @@
 						    while ($loop->have_posts() ) : $loop->the_post();
 								$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ));
 								$excerpt = apply_filters('the_excerpt', get_post_field('post_excerpt', $post->ID));
-								echo "<li style='min-height:670px; background-image:url(" . get_the_post_thumbnail( $post->ID, 'full', $img_attr ) . "); background-size:cover; background-position:center; '>" .  . "</li>";
+								echo "<li style='min-height:670px; background-image:url(" . get_the_post_thumbnail( $post->ID, 'full', $img_attr ) . "); background-size:cover; background-position:center; '> nothing </li>";
 							endwhile;
 					        
 					        wp_reset_query();
