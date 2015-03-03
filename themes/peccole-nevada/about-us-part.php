8f69a287464e1bdab2da80b498180621bedc2a1b
noncasus <noncasus@gmail.com>
2015-03-03 16:45:37 -0600

    about us box
    adds structure and style with dummy image

--- themes/peccole-nevada/about-us-part.php
+++ themes/peccole-nevada/about-us-part.php
@@ -1,1 +1,26 @@
-about us image and floating box go here
+<div class="about-us-banner">
+	<div class="about-us-box-wrapper">
+		<div class="about-us-box clearfix">
+			<div class="col-sm-4">
+				<h3>Founder</h3>
+				<span></span>
+				<h3>President Emeritus</h3>
+				<span></span>
+				<h3>Chief Executive Officer</h3>
+				<span></span>
+			</div>
+			<div class="col-sm-4">
+				<h3>Principals</h3>
+				<span></span>
+				<h3>Board of Directors</h3>
+				<span></span>
+			</div>
+			<div class="col-sm-4">
+				<h3>Chief Financial Officer</h3>
+				<span></span>
+				<h3>Property Manager</h3>
+				<span></span>
+			</div>
+		</div>
+	</div>
+</div>
